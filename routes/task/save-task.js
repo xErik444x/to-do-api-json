b89c0506a0414e0e2cdb32dd76e53749092b5359
xErik444x <erikschwerdt18@gmail.com>
2023-09-16 04:10:35 -0300

    se removieron logs.

--- routes/task/save-task.js
+++ routes/task/save-task.js
@@ -8,11 +8,9 @@ const { randomUUID } = require('crypto');
 router.use(checkBodyPost);
 router.use(checkIfTaskNameExists);
 router.post('/', function(req, res) {
-    console.log("llega al post")
     const dataPath = path.join(__dirname, '../../data/tasks.json');
     const fileContent = fs.readFileSync(dataPath, 'utf-8');
     const data = JSON.parse(fileContent);
-    console.log(`🐧 -> file: save-task.js:16 -> router.post -> data:`, data)
     const newObject = req.body;
     data.push({...newObject, id:randomUUID()});
     const updatedContent = JSON.stringify(data, null, 2); // El tercer argumento (2) es para dar formato legible al JSON
