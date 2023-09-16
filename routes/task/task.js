892c182dd0762cfc3eb06cdeb914069d8c68a317
xErik444x <erikschwerdt18@gmail.com>
2023-09-16 13:21:07 -0300

    renamed save-task to post-task

--- routes/task/task.js
+++ routes/task/task.js
@@ -2,13 +2,13 @@ var {Router} = require('express');
 var router = Router();
 
 const getTasks = require("./get-task")
-const saveTask = require("./save-task")
+const postTask = require("./post-task")
 const deleteTask = require("./delete-task")
 const {checkIfJsonExists} = require("../../middlewares/check-data")
 
 router.use(checkIfJsonExists);
 router.get('/', getTasks);
-router.post('/', saveTask);
+router.post('/', postTask);
 router.delete('/:taskId', deleteTask);
 
 
