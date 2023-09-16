7490c9effcd7506bae04e82418fe9d3dd3e7d021
xErik444x <erikschwerdt18@gmail.com>
2023-09-16 13:16:07 -0300

    changed status codes and pin volta version

--- routes/task/delete-task.js
+++ routes/task/delete-task.js
@@ -9,11 +9,12 @@ router.delete('/:idTask', function(req, res) {
     const dataPath = path.join(__dirname, '../../data/tasks.json');
     const fileContent = fs.readFileSync(dataPath, 'utf-8');
     const data = JSON.parse(fileContent);
-    const encontrado = data.findIndex((task) => task.id == idTask)
-    if(encontrado !== -1){
-        data[encontrado].delete = true
-        data[encontrado].updated = new Date()
+    const encontrado = data.findIndex((task) => task.delete ? false : task.id === idTask)
+    if(encontrado === -1){
+        return  res.sendStatus(404);
     }
+    data[encontrado].delete = true
+    data[encontrado].updated = new Date()
     const updatedContent = JSON.stringify(data, null, 2); // El tercer argumento (2) es para dar formato legible al JSON
     fs.writeFileSync(dataPath, updatedContent);
     res.sendStatus(204);
